51cc03a27073924f18866a44dbc93acad1a94efe
Peter <pkurlak@gmail.com>
2013-10-21 22:44:42 -0500

    Initial upload

---
+++ backend/SnaptorService/CustomApplicationContext.cs
@@ -0,0 +1,43 @@
+﻿using SnaptorService.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnaptorService
+{
+    public sealed class CustomApplicationContext : ApplicationContext
+    {
+        private IContainer _components;
+        private NotifyIcon _notifyIcon;
+
+        public CustomApplicationContext()
+        {
+            this.InitializeContext();
+
+            var hook = new KeyboardHook();
+            hook.KeyPressed += (sender, e) =>
+                {
+                    MessageBox.Show("pressed");
+                };
+
+            hook.RegisterHotKey(ModifierKeys.None, Keys.PrintScreen);
+        }
+
+        private void InitializeContext()
+        {
+            _components = new Container();
+            _notifyIcon = new NotifyIcon(_components)
+            {
+                ContextMenuStrip = new ContextMenuStrip(),
+                Icon = Resources.Raptor,
+                Text = "Change me",
+                Visible = true
+            };
+        }
+    }
+}
---
+++ backend/SnaptorService/Program.cs
@@ -0,0 +1,39 @@
+﻿using MutexManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnaptorService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            if (!SingleInstance.Start()) 
+            { 
+                return; 
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            try
+            {
+                var applicationContext = new CustomApplicationContext();
+                Application.Run(applicationContext);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Program terminated unexpectedly",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            SingleInstance.Stop();
+        }
+    }
+}
